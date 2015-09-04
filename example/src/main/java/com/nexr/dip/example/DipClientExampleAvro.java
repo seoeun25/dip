7da613d15d7f6f7cf88aa22d6419b715a0769a36
Azrael Seoeun Park <seoeun25@gmail.com>
2015-09-04 23:24:09 +0900

    Create DIP example module

--- example/src/main/java/com/nexr/dip/example/DipClientExampleAvro.java
+++ example/src/main/java/com/nexr/dip/example/DipClientExampleAvro.java
@@ -5,26 +5,31 @@ import com.nexr.dip.client.DipClient;
 import com.nexr.dip.conf.Configurable;
 import com.nexr.dip.record.DipRecordBase;
 import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Properties;
 
+/**
+ * Example of the DipClient usage that send the message of which format is avro.
+ */
 public class DipClientExampleAvro {
 
     private DipClient client;
+    private String topic;
 
-    public DipClientExampleAvro(String baseUrl, Properties conf) throws DipException {
+    public DipClientExampleAvro(String baseUrl, String topic, Properties conf) throws DipException {
 
-        client = new DipClient(baseUrl, DipClient.MESSAGE_TYPE.AVRO, conf);
+        this.topic = topic;
+        client = new DipClient(baseUrl, topic, DipClient.MESSAGE_TYPE.AVRO, conf);
         client.start();
     }
 
     public static void main(String... args) {
 
         String baseUrl = "localhost:9092";
-        String schemaRegistryUrl = "http://localhost:18181/repo";
+        String topic = "employee";
+        String schemaRegistryUrl = "http://localhost:18181/schemarepo";
         if (args.length != 0) {
             baseUrl = args[0];
         }
@@ -33,17 +38,11 @@ public class DipClientExampleAvro {
 
         DipClientExampleAvro example = null;
         try {
-            example = new DipClientExampleAvro(baseUrl, conf);
+            example = new DipClientExampleAvro(baseUrl, topic, conf);
 
             String srcInfo = "employee";
-            example.printSchema(srcInfo);
-            example.createTestData(srcInfo);
             Thread.sleep(100);
-            example.createTestData2(srcInfo);
-            Thread.sleep(100);
-            example.createTestData3(srcInfo);
-            Thread.sleep(100);
-            example.createTestData4(srcInfo);
+            example.sendAvroFormatData(srcInfo);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -60,100 +59,24 @@ public class DipClientExampleAvro {
         System.out.println(client.getSchemaFields(srcInfo));
     }
 
-    public void createTestData(String srcInfo) {
-        Map<String, Object> fieldData = new HashMap<String, Object>();
+    public void sendAvroFormatData(String srcInfo) {
 
-        List<Schema.Field> fieldList = client.getSchemaFields(srcInfo);
-
-        for (Schema.Field field : fieldList) {
-            if (field.name().equals("wrk_dt")) {
-                fieldData.put("wrk_dt", Long.valueOf(System.currentTimeMillis()));
-            } else if (field.name().equals("src_info")) {
-                fieldData.put("src_info", "gpx_port");
-            } else if (field.name().equals("nescode")) {
-                fieldData.put("nescode", "nescode-1-" + System.currentTimeMillis());
-            } else if (field.name().equals("port")) {
-                fieldData.put("port", "1010");
-            } else if (field.name().equals("equip_ip")) {
-                fieldData.put("equip_ip", "ip:211.211.211.211");
-            } else {
-                fieldData.put(field.name(), null);
-            }
-        }
-        try {
-            client.send(srcInfo, fieldData);
-        } catch (DipException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void createTestData2(String srcInfo) {
-        Map<String, Object> fieldData = new HashMap<String, Object>();
-        fieldData.put("nescode", "nescode-2-" + System.currentTimeMillis());
-        fieldData.put("port", "1010");
-        fieldData.put("setup_val", "hahahoho");
-        fieldData.put("nego", "hello");
-        fieldData.put("setup_speed", "speed-2");
-        fieldData.put("curnt_speed", "speed-current");
-        fieldData.put("mac_cnt", null);
-        fieldData.put("downl_speed_val", null);
-        fieldData.put("etc_extrt_info", null);
-        fieldData.put("wrk_dt", Long.valueOf(System.currentTimeMillis()));
-        fieldData.put("src_info", srcInfo);
-        fieldData.put("equip_ip", "dip-ip");
-
-
-        try {
-            client.send(srcInfo, fieldData);
-        } catch (DipException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void createTestData3(String srcInfo) {
-
-        Map<String, Object> fieldData = new HashMap<String, Object>();
-        fieldData.put("nescode", "nescode-3-" + System.currentTimeMillis());
-        fieldData.put("port", "1010");
-        fieldData.put("setup_val", "hahahoho-333");
-        fieldData.put("nego", "hello");
-        fieldData.put("setup_speed", "speed-3");
-        fieldData.put("curnt_speed", "speed-current");
-        fieldData.put("mac_cnt", null);
-        fieldData.put("downl_speed_val", null);
-        fieldData.put("etc_extrt_info", null);
-        fieldData.put("wrk_dt", Long.valueOf(System.currentTimeMillis()));
-        fieldData.put("src_info", srcInfo);
-        fieldData.put("equip_ip", "dip-ip");
-
-        DipRecordBase dipRecordBase = new DipRecordBase(client.getSchema(srcInfo), fieldData);
-        try {
-            client.send(dipRecordBase);
-        } catch (DipException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void createTestData4(String srcInfo) {
-
-        DipRecordBase dipRecordBase = new DipRecordBase(client.getSchema(srcInfo));
-
-        Map<String, Object> fieldData = new HashMap<String, Object>();
+        Schema schema = client.getSchema(srcInfo);
 
         for (int i = 0; i < 10; i++) {
-            fieldData = new HashMap<String, Object>();
-            fieldData.put("name", "seoeun 01-" + System.currentTimeMillis());
-            fieldData.put("favorite_number", 11);
-            fieldData.put("favorite_color", "ggggg-44");
-            fieldData.put("event_time", Long.valueOf(System.currentTimeMillis()));
+            GenericRecord record = new GenericData.Record(schema);
+            record.put("name", i + "::seoeun-33-hello-azrael");
+            record.put("favorite_number", String.valueOf(i));
+            record.put("wrk_dt", System.currentTimeMillis());
+            record.put("src_info", "employee");
 
-            dipRecordBase.putFields(fieldData);
             try {
+                DipRecordBase<GenericRecord> dipRecordBase = new DipRecordBase(srcInfo, record, DipClient.MESSAGE_TYPE.AVRO);
                 client.send(dipRecordBase);
-            } catch (DipException e) {
-                e.printStackTrace();
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace(System.out);
             }
-
         }
 
     }
--- example/src/main/java/com/nexr/dip/example/DipClientExampleText.java
+++ example/src/main/java/com/nexr/dip/example/DipClientExampleText.java
@@ -4,25 +4,32 @@ import com.nexr.dip.DipException;
 import com.nexr.dip.client.DipClient;
 import com.nexr.dip.conf.Configurable;
 import com.nexr.dip.record.DipRecordBase;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericRecord;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Properties;
 
+/**
+ * Example of the DipClient usage that send the message of which format is text.
+ */
 public class DipClientExampleText {
 
     private DipClient client;
 
-    public DipClientExampleText(String baseUrl, Properties conf) throws DipException {
+    private String topic;
 
-        client = new DipClient(baseUrl, DipClient.MESSAGE_TYPE.TEXT, conf);
+    public DipClientExampleText(String baseUrl, String topic, Properties conf) throws DipException {
+
+        this.topic = topic;
+        client = new DipClient(baseUrl, topic, DipClient.MESSAGE_TYPE.TEXT, conf);
         client.start();
     }
 
     public static void main(String... args) {
 
         String baseUrl = "localhost:9092";
-        String schemaRegistryUrl = "http://localhost:18181/repo";
+        String topic = "hello";
+        String schemaRegistryUrl = "http://localhost:18181/schemarepo";
         if (args.length != 0) {
             baseUrl = args[0];
         }
@@ -31,10 +38,10 @@ public class DipClientExampleText {
 
         DipClientExampleText example = null;
         try {
-            example = new DipClientExampleText(baseUrl, conf);
+            example = new DipClientExampleText(baseUrl, topic, conf);
 
             String srcInfo = "hello";
-            example.sendTextFormatTest(srcInfo);
+            example.sendTextFormatData(srcInfo);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,26 +52,23 @@ public class DipClientExampleText {
         }
     }
 
-    public void printSchema(String srcInfo) {
-        System.out.println(client.getSchema(srcInfo));
-        System.out.println("------------------");
-        System.out.println(client.getSchemaFields(srcInfo));
-    }
+    public void sendTextFormatData(String srcInfo) {
 
-    public void sendTextFormatTest(String srcInfo) {
+        long time = System.currentTimeMillis();
 
-        DipRecordBase dipRecordBase = new DipRecordBase(srcInfo);
-        Map<String, Object> fieldData = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            String msg = "==00==" + i + "==hi|" + "bbb|" + "ccc|" + i;
 
-        String msg = "az|" + "bbb|" + "ccc|" + System.currentTimeMillis();
+            GenericRecord record = new GenericData.Record(DipClient.TEXT_FORMAT_SCHEMA);
+            record.put(DipRecordBase.MESSAGE_FIELD, msg);
+            record.put(DipRecordBase.EVENTTIME_FIELD, String.valueOf(time));
 
-        dipRecordBase.put(DipRecordBase.MESSAGE_FIELD, msg);
-        dipRecordBase.put(DipRecordBase.EVENTTIME_FIELD, System.currentTimeMillis());
-
-        try {
-            client.send(dipRecordBase);
-        } catch (DipException e) {
-            e.printStackTrace();
+            try {
+                DipRecordBase<String> dipRecordBase = new DipRecordBase(srcInfo, record, DipClient.MESSAGE_TYPE.TEXT);
+                client.send(dipRecordBase);
+            } catch (DipException e) {
+                e.printStackTrace();
+            }
         }
     }
 
