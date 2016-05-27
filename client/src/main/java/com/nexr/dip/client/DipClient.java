a3687bded5020f9166627dd3a76c1c62e1e0a23d
Azrael Seoeun Park <seoeun25@gmail.com>
2016-05-27 13:10:46 +0900

    Increase batchsize, buffermemory for producer

--- client/src/main/java/com/nexr/dip/client/DipClient.java
+++ client/src/main/java/com/nexr/dip/client/DipClient.java
@@ -275,6 +275,8 @@ public class DipClient<D> implements Configurable {
             map.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, ByteArraySerializer.class.getName());
             map.put(ProducerConfig.BLOCK_ON_BUFFER_FULL_CONFIG, "false");
             map.put(ProducerConfig.ACKS_CONFIG, "1");
+            map.put(ProducerConfig.BATCH_SIZE_CONFIG, "327680");
+            map.put(ProducerConfig.BUFFER_MEMORY_CONFIG, "671088640");
             map.put(Configurable.SCHEMAREGISTRY_CLASS, "com.nexr.schemaregistry.AvroSchemaRegistry");
             map.put(KafkaAvroMessageEncoder.KAFKA_MESSAGE_CODER_SCHEMA_REGISTRY_CLASS, "com.nexr.schemaregistry.AvroSchemaRegistry");
             map.put("partitioner.class", "kafka.producer.ByteArrayPartitioner");
