4364e3c5b603f8c04a1878f02befa49809667e81
Azrael Seoeun Park <seoeun25@gmail.com>
2016-06-02 15:54:43 +0900

    Remove stop fucntion from schema-registry start shell

--- schema-registry/bin/env.sh
+++ schema-registry/bin/env.sh
@@ -7,4 +7,10 @@
 ## SCHEMA_REPO_LOG_DIR
 #LOG_DIR=/home/sdip/library/schema-registry-repo/logs
 
+## DIP_LOG_ROOTLOGGER
+#LOG_ROOTLOGGER="INFO,DAILY"
+
+## DIP_JAVA_OPT
+#JAVA_OPT=-Xms2048m -Xmx4096m
+
 
--- schema-registry/bin/schemarepo.sh
+++ schema-registry/bin/schemarepo.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 #
 
-if [ $# -le 0 ]; then
-  echo "Usage: schemarepo.sh (start|stop)"
-  exit 1
-fi
-
-command=$1
-shift
-
 PRG="${0}"
 BASEDIR=`dirname ${PRG}`
 BIN_DIR=$BASEDIR
@@ -25,11 +17,14 @@ if [ "$LOG_DIR" == "" ]; then
 fi
 mkdir -p $LOG_DIR
 
+if [ "$LOG_ROOTLOGGER" == "" ]; then
+    LOG_ROOTLOGGER="INFO,console"
+fi
+
 if [ "$CONF_DIR" == "" ]; then
     CONF_DIR=$SCHEMA_REPO_HOME/conf
 fi
 
-CLASS_PATH=""
 SCHEMA_REPO_CONF=$CONF_DIR
 # prepend conf dir to classpath
 if [ -n "$SCHEMA_REPO_CONF" ]; then
@@ -38,61 +33,9 @@ fi
 
 CLASS_PATH=${CLASS_PATH}:${LIB}/'*'
 
-if [ "$SCHEMA_REPO_PID_DIR" = "" ]; then
-  SCHEMA_REPO_PID_DIR=/tmp
+if [ "$JAVA_OPT" == "" ]; then
+    JAVA_OPT="-Xms2048m -Xmx4096m"
 fi
-log=$LOG_DIR/schemarepo.out
-pid=$SCHEMA_REPO_PID_DIR/schemarepo.pid
-STOP_TIMEOUT=${STOP_TIMEOUT:-3}
 
-JAVA_OPT="-Xms2048m -Xmx4096m"
+java ${JAVA_OPT} -cp ${CLASS_PATH} -Dlog.dir=${LOG_DIR} -Dlog.rootLogger=${LOG_ROOTLOGGER} com.nexr.server.DipSchemaRepoServer start
 
-module=DipSchemaRepoServer
-case $command in
-
-  (start)
-    [ -w "$SCHEMA_REPO_PID_DIR" ] ||  mkdir -p "$SCHEMA_REPO_PID_DIR"
-
-    if [ -f $pid ]; then
-      if kill -0 `cat $pid` > /dev/null 2>&1; then
-        echo $module running as process `cat $pid`.  Stop it first.
-        exit 1
-      fi
-    fi
-
-    echo starting $module logging to $log
-    java ${JAVA_OPT} -cp ${CLASS_PATH} -Dlog.dir=${LOG_DIR} com.nexr.server.DipSchemaRepoServer ${command} > "$log" 2>&1 < /dev/null &
-
-    echo $! > $pid
-    TARGET_PID=`cat $pid`
-    echo starting as process $TARGET_PID
-    echo ""
-    ;;
-  (stop)
-
-    if [ -f $pid ]; then
-      TARGET_PID=`cat $pid`
-      if kill -0 $TARGET_PID > /dev/null 2>&1; then
-        echo stopping $module
-        kill $TARGET_PID
-        sleep $STOP_TIMEOUT
-        if kill -0 $TARGET_PID > /dev/null 2>&1; then
-          echo "$module did not stop gracefully after $STOP_TIMEOUT seconds: killing with kill -9"
-          kill -9 $TARGET_PID
-        fi
-      else
-        echo no $module to stop
-      fi
-      rm -f $pid
-    else
-      echo no $module to stop
-    fi
-    echo ""
-    ;;
-
-  (*)
-    echo $usage
-    exit 1
-    ;;
-
-esac
