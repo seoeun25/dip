b4b01c3e4a48c274ea7eb0a64bd23293ab7bcfaf
Azrael Seoeun Park <seoeun25@gmail.com>
2016-06-13 17:13:14 +0900

    Revise run-example

--- tool/bin/run-employee.sh
+++ tool/bin/run-employee.sh
@@ -2,16 +2,21 @@
 #
 
 if [ $# -le 0 ]; then
-  echo "Usage: run-employee.sh {borker}, {port}, {topic}, {parition}, {offset}, {maxread}"
+  echo "Usage: run-employee.sh {borker}"
 
   exit 1
 fi
 
-## insert schema of employee into schemainfo table
-mysql>
-INSERT INTO schemainfo (created, name, schemaStr) VALUES(now(), 'employee', '{"type":"record","name":"employee","namespace":"com.nexr.dip.avro.schema","fields":[{"name":"name","type":"string"},{"name":"favorite_number","type":["int","null"]},{"name":"favorite_color","type":["string","null"]},{"name":"wrk_dt","type":"long"},{"name":"srcinfo","type":"string"},{"name":"header","type":{"type":"record","name":"headertype","fields":[{"name":"time","type":"long"}]}}]}');
+###### insert schema of employee into schemainfo table
+# mysql>
+# INSERT INTO schemainfo (created, name, schemaStr) VALUES(now(), 'employee', '{"type":"record","name":"employee", "namespace":"com.nexr.dip.avro.schema","fields":[{"name":"name","type":"string"},{"name":"favorite_number","type":["string","null"]},{"name":"favorite_color","type":["string","null"]},{"name":"wrk_dt","type":"long"},{"name":"src_info","type":"string"},{"name":"header","type":{"type":"record","name":"headertype","fields":[{"name":"time","type":"long"}]}}]}');
+#
+######
 
 ## send event(employee) to kafka
-$ bin/generate-message.sh --config broker=localhost:9092
+BROKER=$1
+echo $BROKER
+COMMAND=bin/generate-message.sh --config broker=$BROKER
+`$COMAND`
 
 ## DIP will upload the event of employee on HDFS
